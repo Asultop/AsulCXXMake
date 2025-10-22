387bf9b2abaf1b6e9fa062f2213861d96db10fe2
Alivn <Alivn@Asul.top>
2025-10-22 20:45:13 +0800

    [FUNC]  Add program_suffix to distinguish compiler

--- AsulCXXMake.cpp
+++ AsulCXXMake.cpp
@@ -7,6 +7,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 #include "AsulFormatString.h"
 #include "Def.h"
@@ -21,16 +22,42 @@ using namespace AsulKit::FileTools;
 
 using std::cout;
 
+std::map<FileType, std::string> FileTypeSuffix = {
+    {FileType::C, ".c"},
+    {FileType::CPP, ".cpp"},
+};
+
+
+bool getStatusYesOrNo(char getIn, bool defaultYes=true, std::function<void()> onYes=[](){}, std::function<void()> onNo=[](){}) {
+    switch (getIn) {
+    case 'Y':
+    case 'y':
+        onYes();
+        return true;
+    case 'N':
+    case 'n':
+        onNo();
+        return false;
+    case '\n':
+    default:
+        if (defaultYes) {
+            onYes();
+        } else {
+            onNo();
+        }
+        return defaultYes;
+    }
+}
 bool build(const std::string projectNameSource) {
   std::string projectName = getPureContent(projectNameSource);
-  std::string SUFFIX;
+  std::string SUFFIX,ProgramSUFFIX;
   FileType type = getType(projectNameSource);
-  if (type == FileType::C)
-    SUFFIX = ".c";
-  else
-    SUFFIX = ".cpp";
+  SUFFIX = FileTypeSuffix[type];
+  ProgramSUFFIX = SUFFIX;
+  std::replace(ProgramSUFFIX.begin(), ProgramSUFFIX.end(), '.', '_');
+  
   std::string cmd = CPP_COMPILER + COMPILER_FLAGS + projectName + SUFFIX + " " +
-                    OUTPUT(projectName);
+                    OUTPUT(projectName+ProgramSUFFIX);
   cout << f("(INFO) 执行构建命令: {DARK_GRAY} \n", cmd);
 
   auto Begin = std::chrono::high_resolution_clock::now();
@@ -40,7 +67,6 @@ bool build(const std::string projectNameSource) {
     if (fileStampStatus.first == false) {
       cout << f("(WARN) 获取文件修改时间错误！\n");
     } else {
-      // auto writeReturn = writeToFile(CACHE_TIME_INFO, fileStampStatus.second);
       json stampDoc;
       if(fileExist(CACHE_TIME_INFO)) {
          stampDoc = json::parse(getFileContent(CACHE_TIME_INFO));
@@ -52,7 +78,6 @@ bool build(const std::string projectNameSource) {
                   writeReturn.second);
       }
     }
-
     cout << f("(SUCCESS) 构建 {YELLOW} 完成\n", projectName);
     cout << f("(INFO) 耗时 {DARK_GRAY} s\n", std::chrono::duration<double>(std::chrono::high_resolution_clock::now()-Begin).count());
     return true;
@@ -64,8 +89,11 @@ bool build(const std::string projectNameSource) {
 int run(const std::string projectNameSource) {
 
   FileType fileType = getType(projectNameSource);
+  std::string ProgramSUFFIX = FileTypeSuffix[fileType];
+  std::replace(ProgramSUFFIX.begin(), ProgramSUFFIX.end(), '.', '_');
+
   std::string inStream = "";
-  std::string projectName = getPureContent(projectNameSource);
+  std::string projectName = getPureContent(projectNameSource)+ProgramSUFFIX;
   bool hasOutStream = false;
   std::string lastStamp;
   std::pair<bool, std::string> fileStampStatus;
@@ -81,64 +109,56 @@ int run(const std::string projectNameSource) {
       return BadFile;
     }
     fileStampStatus = getFileModificationTime(projectNameSource);
-    if (!fileStampStatus.first) {
+    if (!fileStampStatus.first) { 
       cout<<f("(ERROR) 获取文件修改时间失败\n");
     }
   }
-  if (fileExist((getPureContent(projectName) + EXE_SUFFIX).c_str())) {
+  if (fileExist(((projectName) + EXE_SUFFIX).c_str())) {
     if (fileStampStatus.second != lastStamp) {
       cout << f("(INFO) 源代码 {YELLOW} 似乎已修改但未编译，是否立刻编译？\n", projectNameSource);
       cout << f("(INFO) 上次编译时间: {DARK_GRAY}\n(ASK_Y): ", lastStamp);
       char getIn = getchar();
       int buildReturnStatus = -1;
-      switch (getIn) {
-        case 'Y':
-        case 'y':
-        case '\n':
+      getStatusYesOrNo(getIn, true, [&]() {
           buildReturnStatus = build(projectNameSource);
-          break;
-        default:
-        case 'N':
-        case 'n':
-          break;
-      }
+      },[&](){
+          buildReturnStatus = 0;
+      });
       if (buildReturnStatus == 0)
         return BuildErr;
     } else {
       cout << f("(INFO) 上次编译时间: {DARK_GRAY}\n", lastStamp);
     }
   }
-  if(lastStamp.empty()){
-      cout << f("(WARN) 未找到缓存时间文件，但已存在可执行文件，是否直接执行并记录当前时间为修改时间\n(ASK_Y) ");
-      char getIn = getchar();
-      switch (getIn) {
-        default:
-        case 'Y':
-        case 'y':
-          {
-              auto fileStampStatus = getFileModificationTime(projectNameSource);
-              if (fileStampStatus.first == false) {
-                  cout << f("(WARN) 获取文件修改时间错误！\n");
-              } else {
-                  // auto writeReturn = writeToFile(CACHE_TIME_INFO, fileStampStatus.second);
-                  json stampDoc;
-                  if(fileExist(CACHE_TIME_INFO)) {
-                     stampDoc = json::parse(getFileContent(CACHE_TIME_INFO));
-                  }
-                  stampDoc[projectNameSource] = fileStampStatus.second;
-                  auto writeReturn = writeToFile(CACHE_TIME_INFO, stampDoc.dump(4));
-                  if (!writeReturn.first) {
-                    cout << f("(WARN) 无法写入缓存文件 : {DARK_GRAY}\n",
-                              writeReturn.second);
-                  }
-              }
-          }
-          break;
-        case 'N':
-        case 'n':
+  if(fileExist((projectName+ EXE_SUFFIX).c_str()) && lastStamp.empty()){
+      cout << f("(WARN) 未找到缓存时间文件，但已存在可执行文件，是否直接执行？\n(ASK_Y) ");
+      getStatusYesOrNo(getchar(), true, []() {
+          // do nothing, continue
+      }, []() {
           cout << f("(INFO) 用户已取消运行\n");
           return Accepted;
-      }
+      });
+      cout << f("(INFO) 写入缓存时间？\n (ASK_N) ");
+      getStatusYesOrNo(getchar(), false, [&]() {
+          auto fileStampStatus = getFileModificationTime(projectNameSource);
+          if (fileStampStatus.first == false) {
+              cout << f("(WARN) 获取文件修改时间错误！\n");
+          } else {
+              json stampDoc;
+              if(fileExist(CACHE_TIME_INFO)) {
+                 stampDoc = json::parse(getFileContent(CACHE_TIME_INFO));
+              }
+              stampDoc[projectNameSource] = fileStampStatus.second;
+              auto writeReturn = writeToFile(CACHE_TIME_INFO, stampDoc.dump(4));
+              if (!writeReturn.first) {
+                cout << f("(WARN) 无法写入缓存文件 : {DARK_GRAY}\n",
+                          writeReturn.second);
+              }
+          }
+      },[&](){
+          cout << f("(INFO) 未写入文件\n");
+          return Accepted;
+      });
   }
   if (fileExist((projectName + FILE_IN_SUFFIX).c_str()))
     inStream = " < " + projectName + FILE_IN_SUFFIX;
@@ -380,17 +400,13 @@ int main(int argc, char *argv[]) {
              arg.length() >= 2 && (arg[0] == '-' && arg[1] == '-')) {
     cout << f("(WARN) {YELLOW} ：看起来似乎是一个选项，仍要将其当作源文件吗? (ASK_N) \n",arg);
     char getIn = getchar();
-    switch (getIn) {
-    case 'Y':
-    case 'y':
-      break;
-    default:
-    case 'N':
-    case 'n':
-      // printInfo("用户已取消 \n");
-      cout<< f("(INFO) 用户已取消");
-      return Accepted;
-    }
+
+    getStatusYesOrNo(getIn, false, []() {
+        // do nothing, continue
+    }, []() {
+        cout << f("(INFO) 用户已取消\n");
+        exit(Accepted);
+    });
   }
 
   std::string lastBuild = "";
@@ -400,8 +416,6 @@ int main(int argc, char *argv[]) {
       json lastBuildDoc=json::parse(lastBuild);
       lastBuild=lastBuildDoc[PROJECT_NAME].get<std::string>();
     } catch (const std::runtime_error &e) {
-      // printErr(e.what());
-      // print("\n");
       cout << f("(ERROR) {}\n",e.what());
       return BadFile;
     }
@@ -409,18 +423,9 @@ int main(int argc, char *argv[]) {
   if (arg == "clear") {
     std::string lastBuildName = getPureContent(lastBuild);
     if (!folderExist(CACHE_DIR_NAME)) {
-      // PrintMap<std::string>()
-      //     .append(LogLevel::Info)
-      //     .append("无上次构建记录，无需清理 ")
-      //     .append(CACHE_DIR_NAME, ConsoloColor::Yellow)
-      //     .printMap();
       cout << f("(INFO) 无上次构建记录，无需清理 {YELLOW}\n",CACHE_DIR_NAME);
       return Accepted;
     }
-    // if (!fileExist(lastBuild.c_str())){
-    //     printInfo("构建文件不存在，无需清理\n");
-    //     return Accepted;
-    // }
     std::string cmd_rm_build_file =
         RM_FILE + FILE_PREFIX + lastBuildName + EXE_SUFFIX;
     std::string cmd_rm_cache_dir = RM_FOLDER + FILE_PREFIX + CACHE_DIR_NAME;
@@ -430,35 +435,17 @@ int main(int argc, char *argv[]) {
       errCount += systemSilent(cmd_rm_cache_dir.c_str());
     }
     if (fileExist(lastBuildName.c_str())) {
-      // PrintMap<std::string>()
-      //     .append("执行清理命令：")
-      //     .append(cmd_rm_build_file, ConsoloColor::LightGray)
-      //     .printMap();
       cout << f("(INFO) 执行清理命令：{DARK_GRAY}",cmd_rm_build_file);
-
       errCount += systemSilent(cmd_rm_build_file.c_str());
     }
     if (errCount == 0) {
-      // PrintMap<std::string>()
-      //     .append(LogLevel::Success)
-      //     .append("清理存在的项目：")
-      //     .append(lastBuildName, ConsoloColor::Yellow)
-      //     .append(" 完成")
-      //     .printMap();
       cout << f("(SUCCESS) 清理存在的项目：{YELLOW} 完成\n", lastBuildName);
       return Accepted;
     }
-    // PrintMap<std::string>()
-    //     .append(LogLevel::Warn)
-    //     .append("清理完成，但出现 ")
-    //     .append(std::to_string(errCount), ConsoloColor::Red)
-    //     .append(" 个错误！")
-    //     .printMap();
     cout << f("(WARN) 清理完成，但出现 {RED} 个错误！\n", errCount);
     return BadFile;
   } else if (arg == "make") {
     if (lastBuild.empty()) {
-      // printErr("无上次构建记录，无法构建\n");
       cout << f("(ERROR) 无上次构建记录，无法构建\n");
       return BuildErr;
     }
@@ -468,7 +455,6 @@ int main(int argc, char *argv[]) {
     return Accepted;
   } else if (arg == "run") {
     if (lastBuild.empty()) {
-      // printErr("无上次构建记录，无法运行\n");
       cout << f("(ERROR) 无上次构建记录，无法运行\n");
       return BuildErr;
     }
@@ -476,96 +462,54 @@ int main(int argc, char *argv[]) {
     if (fileExist((lastBuildPure + EXE_SUFFIX).c_str())) {
       return run(lastBuild);
     } else {
-      // PrintMap<std::string>()
-      //     .append(LogLevel::Warn)
-      //     .append("暂未构建 ")
-      //     .append(lastBuildPure, ConsoloColor::Yellow)
-      //     .append(" 是否立刻构建？\n(Y/" + std::string(UNDERLINE) + "N" +
-      //             std::string(RESET) + "): ")
-      //     .printMap(false);
       cout << f("(WARN) 暂未构建 {YELLOW} 是否立刻构建？\n(ASK_N):", lastBuildPure);
       char getIn = getchar();
-      switch (getIn) {
-      case 'y':
-      case 'Y':
-        if (!build(lastBuild))
-          return BuildErr;
-        return run(lastBuild);
-      default:
-      case 'n':
-      case 'N':
-        // printInfo(std::string("用户已取消 \n"));
-        cout << f("(INFO) 用户已取消 \n");
-      }
+      getStatusYesOrNo(getIn, false, [&]() {
+          if (!build(lastBuild))
+              return BuildErr;
+          return run(lastBuild);
+      },[&](){
+          cout << f("(INFO) 用户已取消 \n");
+          return Accepted;
+      });
     }
     return Accepted;
   } else if (arg == "remake") {
     if (lastBuild.empty()) {
-      // printErr("无上次构建记录，无法重构\n");
       cout << f("(ERROR) 无上次构建记录，无法重构\n");
       return BuildErr;
     }
     std::string lastBuildPure = getPureContent(lastBuild);
 
     if (!fileExist(std::string(lastBuildPure + EXE_SUFFIX).c_str()))
-      // PrintMap<std::string>()
-      //     .append(LogLevel::Warn)
-      //     .append("不存在构建文件，此时推荐使用")
-      //     .append(" make ", ConsoloColor::Yellow)
-      //     .append("命令，以减少错误警报！")
-      //     .printMap();
       cout << f("(WARN) 不存在构建文件，此时推荐使用 {YELLOW} 命令，以减少错误警报！\n", "make");
     if (!build(lastBuild))
       return BuildErr;
     return run(lastBuild);
   } else if (arg == "status") {
     if (lastBuild.empty()) {
-      // printInfo("暂未构建过项目 （暂存区干净）\n");
       cout << f("(INFO) 暂未构建过项目 （暂存区干净）\n");
       return Accepted;
     }
-    // PrintMap<std::string>()
-    //     .append(LogLevel::Success)
-    //     .append("上次构建的项目：")
-    //     .append(lastBuild)
-    //     .printMap();
     cout << f("(SUCCESS) 上次构建的项目：{YELLOW} \n", lastBuild);
     return Accepted;
   }
 
   if (getType(arg) != FileType::C && getType(arg) != FileType::CPP) {
-    // PrintMap<std::string>()
-    //     .append(LogLevel::Err)
-    //     .append(arg, ConsoloColor::Yellow)
-    //     .append(" 类型无效")
-    //     .printMap();
     cout << f("(ERROR) {YELLOW} 类型无效\n", arg);
     return TypeErr;
   }
   std::string targetProject = arg;
 
   if (!fileExist((targetProject).c_str())) {
-    // PrintMap<std::string>()
-    //     .append(LogLevel::Err)
-    //     .append("文件 ")
-    //     .append(targetProject, ConsoloColor::Yellow)
-    //     .append(" 不存在！")
-    //     .printMap();
     cout << f("(ERROR) 文件 {YELLOW} 不存在！\n", targetProject);
     return BadFile;
   }
 
   if (!createDirectoryIfNotExists(CACHE_DIR_NAME)) {
-    // PrintMap<std::string>()
-    //     .append(LogLevel::Err)
-    //     .append("无法创建文件夹 :")
-    //     .append(CACHE_DIR_NAME, ConsoloColor::LightGray)
-    //     .printMap();
     cout << f("(ERROR) 无法创建文件夹 : {DARK_GRAY}\n", CACHE_DIR_NAME);
     return EnvErr;
   };
-
-  // auto writeReturn = writeToFile(LAST_BUILD_INFO, targetProject);
   json lastBuildDoc;
   if(fileExist(LAST_BUILD_INFO)) {
      lastBuildDoc = json::parse(getFileContent(LAST_BUILD_INFO));
@@ -573,15 +517,11 @@ int main(int argc, char *argv[]) {
   lastBuildDoc[PROJECT_NAME]=targetProject;
   auto writeReturn = writeToFile(LAST_BUILD_INFO, lastBuildDoc.dump(4));
   if (!writeReturn.first) {
-    // PrintMap<std::string>()
-    //     .append(LogLevel::Warn)
-    //     .append("无法写入缓存文件 :")
-    //     .append(writeReturn.second, ConsoloColor::LightGray)
-    //     .printMap();
     cout << f("(WARN) 无法写入缓存文件 : {DARK_GRAY}\n", writeReturn.second);
   }
-
-  if (!fileExist((getPureContent(arg) + EXE_SUFFIX).c_str()))
+  std::string ProgramSUFFIX = FileTypeSuffix[getType(arg)];
+  std::replace(ProgramSUFFIX.begin(), ProgramSUFFIX.end(), '.', '_');
+  if (!fileExist((getPureContent(arg)+ ProgramSUFFIX + EXE_SUFFIX).c_str()))
     // if(true)
     if (!build(targetProject))
       return BuildErr;
